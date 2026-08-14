e06738192f089ae9744933e210bac5d0739fcdfa
ABS-223 <amadoubatie.sidibe@gmail.com>
2026-08-14 13:54:42 +0000

    créer des test automatiques pour ajouter des test sur la carte, mais sur le mien ça ne fonctionne pas vu qu'il faut enlever les parties de selecteur de dom pour que ça fonctionne  donc j'ai préférer garder le code du test sans pour autant m'assurer qu'il est 100%ok

--- jasmine-standalone-5.1.1/data/cartTest.js
+++ jasmine-standalone-5.1.1/data/cartTest.js
@@ -1,20 +1,41 @@
-import { addToCart,cart,loadFromStorage } from "../../data/cart.js";
+import {addToCart, cart, loadFromStorage} from '../../data/cart.js';
 
-describe ('test suite : add to cart', ()=>{
+describe('test suite: addToCart', () => {
+  it('adds an existing product to the cart', () => {
+    // Pour mock les valeurs des localStorages 
+    // Et ces mock ne sont pas persistants, donc chaque test est indépendant et à besoin d'un nouveau mock
+    spyOn(localStorage, 'setItem');
 
-    it('ajouter un produit deja existant à la carte',()=>{
-    
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);
     });
-    it('ajouter un nouveau produit à la carte',()=>{
-         //Pour moque le return de localStorage et faire en sorte qu'il retourne un empty array
-        spyOn(localStorage, 'getItem').and.callFake(()=>{
-            return JSON.stringify([]);
-        });
-        console.log(localStorage.getItem('cart'));
-        loadFromStorage();
+    loadFromStorage();
 
-        addToCart('83d4ca15-0f35-48f5-b7a3-1ea210004f2e')
-        expect(cart.length).toEqual(1);
-    })
+    addToCart('83d4ca15-0f35-48f5-b7a3-1ea210004f2e');
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual('83d4ca15-0f35-48f5-b7a3-1ea210004f2e');
+    expect(cart[0].quantity).toEqual(2);
+  });
 
-})
+
+
+  it('adds a new product to the cart', () => {
+    spyOn(localStorage, 'setItem');
+
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([]);
+    });
+    loadFromStorage();
+
+    addToCart('83d4ca15-0f35-48f5-b7a3-1ea210004f2e');
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual('83d4ca15-0f35-48f5-b7a3-1ea210004f2e');
+    expect(cart[0].quantity).toEqual(1);
+  });
+});
