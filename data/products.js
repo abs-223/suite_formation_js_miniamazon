dd15e919540cab61723d27c0fbb935e5a301dd54
ABS-223 <amadoubatie.sidibe@gmail.com>
2026-08-18 16:02:58 +0000

    ajout des classes pour les appliances et màj du code

--- data/products.js
+++ data/products.js
@@ -56,6 +56,31 @@ class Product {
       `
     }
   }
+  class Appliance extends Product{
+    instructionsLink;
+    warrantyLink;
+
+    constructor(productDetails){
+      super(productDetails);
+      this.instructionsLink=productDetails.instructionsLink;
+      this.warrantyLink=productDetails.warrantyLink;
+    }
+    extraInfosHTML(){
+      //Manière si on veut vraiment utiliser la méthode du parent
+      // super(extraInfosHTML()); 
+      return `
+
+        <a href="${this.instructionsLink}" target="_blank" >
+        Instructions
+        </a>
+        <a href="${this.warrantyLink}" target="_blank" >
+        Warranty
+        </a>
+      
+      `
+    }
+
+  }
 
 
   //codede test
@@ -174,7 +199,10 @@ export const products = [
       "toaster",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: "appliance",
+    instructionsLink : "https://supersimple.dev/images/appliance-instructions.png",
+    warrantyLink : "https://supersimple.dev/images/appliance-warranty.png",
   },
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
@@ -359,7 +387,10 @@ export const products = [
       "water boiler",
       "appliances",
       "kitchen"
-    ]
+    ],
+    type: "appliance",
+    instructionsLink : "https://supersimple.dev/images/appliance-instructions.png",
+    warrantyLink : "https://supersimple.dev/images/appliance-warranty.png",
   },
   {
     id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
@@ -664,7 +695,10 @@ export const products = [
       "coffeemakers",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: "appliance",
+    instructionsLink : "https://supersimple.dev/images/appliance-instructions.png",
+    warrantyLink : "https://supersimple.dev/images/appliance-warranty.png",
   },
   {
     id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
@@ -724,7 +758,10 @@ export const products = [
       "food blenders",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: "appliance",
+    instructionsLink : "https://supersimple.dev/images/appliance-instructions.png",
+    warrantyLink : "https://supersimple.dev/images/appliance-warranty.png",
   },
   {
     id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
@@ -778,6 +815,9 @@ export const products = [
   if(productsDetails.type==="clothing"){
     return new Clothing (productsDetails);
 
+  }
+  if(productsDetails.type==="appliance"){
+    return new Appliance (productsDetails);
   }
 
   return new Product (productsDetails);
