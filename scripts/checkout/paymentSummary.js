b983dd78bde0aa2317d7994d49d349d3a78bea62
ABS-223 <amadoubatie.sidibe@gmail.com>
2026-08-21 15:36:39 +0000

    create order en utilisant le backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,6 +1,7 @@
 import {cart} from '../../data/cart.js';
 import { getProduct } from '../../data/products.js';
-import { getDeliveryOption } from '../../data/deliveryOptions.js';
+import { getDeliveryOption } from '../../data/deliveryOptions.js'
+import { addOrder } from '../../data/orders.js';
 
 export function renderPaymentSummary(){
     let productPriceCents=0;
@@ -59,7 +60,7 @@ export function renderPaymentSummary(){
             <div class="payment-summary-money">$${(totalCents/100).toFixed(2)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary js-place-order">
             Place your order
           </button>
     `
@@ -68,12 +69,32 @@ export function renderPaymentSummary(){
     document.querySelector('.js-payment-summary').innerHTML= paymentSummaryHTML;
 
 
+    document.querySelector('.js-place-order').addEventListener('click',async ()=>{
+
+      try{
+
+        const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify({
+          cart : cart
+        })
+      })
+
+     const order = await response.json();
+
+     addOrder(order);
+
+      }catch(error){
+
+        console.log('Unexepted error, Try again later')
+
+      }
+      
+      window.location.href = 'orders.html';
+
+    });
+
 } 
-
-export function testJasmine(fioun){
-
-  let result = fioun*5;
-
-  return result;
-
-}
