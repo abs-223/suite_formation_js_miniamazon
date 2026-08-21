0d041f6142a34c2028a924f8bbecd03c8fcac826
ABS-223 <amadoubatie.sidibe@gmail.com>
2026-08-21 09:58:38 +0000

    convertion de promises a asyncawait

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -7,7 +7,29 @@ import {loadCart} from '../data/cart.js'
 // import '../data/backend-practice.js';
 
 
+//async-await
 
+async function loadPage(){
+ 
+    await loadProductsFetch();
+
+    await new Promise ((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        })
+    });
+    
+    renderOrderSummary();
+    renderPaymentSummary();
+
+}
+// la valeur de return sera save dans le then comme parametre comme pour le fetch
+
+loadPage();
+
+
+
+/*
 //Promise.all nous permet d'éxécuter les promises qu'elle contient en même temps sans attendre qu'elles s'éxécutent une à une 
 Promise.all([
     //vu que notre ProductsFetch retourne 
@@ -17,10 +39,12 @@ Promise.all([
             resolve();
         })
     })
-]).then(()=>{
+]) .then(()=>{
     renderOrderSummary();
     renderPaymentSummary();
 })
+
+*/
 
 
 // new Promise((resolve)=>{
